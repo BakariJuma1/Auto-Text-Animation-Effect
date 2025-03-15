72d8bf6566474127a0e482837f67b3160ae9dbd9
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-16 01:09:56 +0300

    accessing an array and using array methods

---
+++ index.js
@@ -0,0 +1,22 @@
+const containerE1 = document.querySelector(".container");
+
+const careers = ["youtuber", "producer", "web developer", "freelancer"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  containerE1.innerHTML = ` 
+  <h1>I am a ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+
+  characterIndex++;
+
+  if (characterIndex === careers[careerIndex].length + 1) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
